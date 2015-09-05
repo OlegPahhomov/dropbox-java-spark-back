6def09dfe0b85b7aae1f3a7121b787e0e9144d04
oleg.pahhomov@gmail.com <lol98nuj>
2015-09-05 12:09:28 +0300

    added filedao, git errors...

---
+++ src/main/java/files/dao/FileDAO.java
@@ -0,0 +1,36 @@
+package files.dao;
+
+import config.AppDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.MapListHandler;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class FileDao {
+    static QueryRunner queryRunner = new QueryRunner();
+
+    public static int saveFileRow(Connection connection, BufferedImage bufferedImageFrom, String fileName) throws IOException, SQLException {
+        PreparedStatement filePs = connection.prepareStatement("INSERT INTO FILE(name, ratio) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        filePs.setString(1, fileName);
+        filePs.setDouble(2, DaoUtil.getRatio(bufferedImageFrom));
+        filePs.executeUpdate();
+        return DaoUtil.getId(filePs);
+    }
+
+    public static int deleteFileRow(Connection connection, Long id) throws SQLException {
+        PreparedStatement filePs = connection.prepareStatement("DELETE FROM FILE WHERE ID=?");
+        filePs.setLong(1, id);
+        return filePs.executeUpdate();
+    }
+
+    public static Object getPictures() throws SQLException {
+        try (Connection connection = AppDataSource.getConnection()) {
+            return queryRunner.query(connection, "SELECT * FROM FILE", new MapListHandler());
+        }
+    }
+}
